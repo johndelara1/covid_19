c3c622abc5ce0d0e8a79bbb22b88883797c78bd6
johndelara1 <johndelara1@gmail.com>
2020-05-22 10:07:37 -0300

    adicionada função que transforma xlsx

--- atualizar_dados_covid.sh
+++ atualizar_dados_covid.sh
@@ -3,7 +3,6 @@ eval "$(conda shell.bash hook)"
 cd ~/git/covid_19/
 conda deactivate
 conda activate covid
-mkdir john
 python raspagem_covid.py
-
-#git add . && git commit -m "adicionada função que transforma xlsx" && git push origin master
+python transform_df.py
+git add . && git commit -m "adicionada função que transforma xlsx" && git push origin master
