7d9999ce353c9b279c40940aff9cf97df280f6f9
johndelara1 <johndelara1@gmail.com>
2020-05-23 13:37:44 -0300

    adicionada função que transforma xlsx

--- teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-eval "$(conda shell.bash hook)"
-cd ~/git/covid_19/
-conda deactivate
-conda activate covid
-python raspagem_covid.py
-python transform_df.py
-git add . && git commit -m "atualização dos dados" && git push origin master
-
